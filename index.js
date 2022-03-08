23b0f4b99277cfbfd0ffeac4ab256b77a04063f0
kpbarasa <65596472+kpbarasa@users.noreply.github.com>
2022-03-08 11:56:16 +0300

    first commit

--- index.js
+++ index.js
@@ -6,21 +6,28 @@ const app = express()
 
 // Help connect to MongoDB database 
 const mongoose = require('mongoose');
+const { render, json } = require('express/lib/response');
+const { default: axios } = require('axios');
 
 require('dotenv').config();
 
 // Set Port 
 const port = process.env.PORT || 5000;
 
+// View engine 
+app.set("view engine", "ejs")
 
-app.use(cors({ origin: 'http://localhost:5000' }));
+// Satic files 
+app.use(express.static("public"))
+
+// Incase of external client 
+// app.use(cors({ origin: 'http://localhost:5000' }));
 
 // parse form data
 app.use(express.urlencoded({extended: false}))
 
 // Parse Json 
 app.use(express.json()) 
-
 
 // Connection String to express atlas and Environment variable.
 const uri = process.env.ATLAS_URI;
@@ -46,55 +53,63 @@ connection.once('open', () => {
 
 // Setup Stripe
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+const stripeSessionId = ""
 
 // List of items we are selling (database or JSON file)
 const storeItems = new Map([
-  [1, { priceInCents: 10000, tittle: "Learn React Today" }],
-  [2, { priceInCents: 15000, tittle: "Learn CSS Today" }],
+  [1, { priceInCents: 1000, tittle: "Learn React Today" }],
+  [2, { priceInCents: 1500, tittle: "Learn CSS Today" }],
 ])
 
-// Post request for /create-checkout-session
+
+// cart items here 
+const items = [ 
+  { id: 1, quantity: 2 },
+  { id: 2, quantity: 3 },
+]
+
 app.post("/create-checkout-session", async (req, res) => {
-    try {
-      // Create a checkout session with Stripe
-      const session = await stripe.checkout.sessions.create({
-        
-        payment_method_types: ["card"],
-
-        // For each item use the id to get it's information
-        // Take that information and convert it to Stripe's format
-        line_items: req.body.items.map(({ id, quantity }) => {
-          console.log(storeItems.get(id).tittle)
-          const storeItem = storeItems.get(id)
-          return {
-            price_data: {
-              currency: "usd",
-              product_data: {
-                name: storeItem,
-              },
-              unit_amount: storeItem.priceInCents,
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: items.map(item => {
+        const storeItem = storeItems.get(item.id)
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: storeItem.tittle,
             },
-            quantity: quantity,
-          }
-        }),
-        // Payment type  
-        mode: "payment",
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        }
+      }),
+      success_url: `${process.env.CLIENT_URL}/success.html`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+    })
+    console.log(session)
+    res.json({ url: session.url })
 
-        //  CLIENT_URL
-        success_url: `${process.env.CLIENT_URL}/success`,
-        cancel_url: `${process.env.CLIENT_URL}/cancel`,
-      })
-      
-      res.json({ url: session.url })
-    } catch (e) {
-      // If there is an error send it to the client
-      res.status(500).json({ error: e.message })
-    }
-  }) 
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+})
   
-app.get("/success", (req, res) => {
-  try {   
-    res.json("success") 
+app.get("/success", async (req, res) => {
+  try {  
+    const options ={
+      headers: {'Authorization': 'Bearer sk_test_SM2z6QB6POe73q1ODaWbseD0008FLkWplY'}
+    }
+
+    const getSession = await axios.get("https://api.stripe.com/v1/checkout/sessions/cs_test_b1g0SplvZoiYPvVg1zWsjRUCCnOi25ojiGW4E3R5ly7zAhfxJfKnpF59vo",options)
+    .then(res => res.data) 
+    console.log(
+      "Total payment: "+getSession.currency +" "+ getSession.amount_total +" "+
+      "Payment type: "+getSession.payment_method_types+" "+
+      "Payment Status: "+getSession.payment_status 
+    )
   } 
   catch (error) {
     res.json(error)
@@ -111,5 +126,8 @@ app.get("/cancle",(req, res) => {
   }
 })
 
+app.get("/",(req, res) => {
+  res.render('index')
+})
 // Port 
 app.listen(port)
